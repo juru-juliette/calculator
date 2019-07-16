8d6055c0b6b72b46edd95dfe21f7055f9001a3ed
juru-juliette <kangabejuliette@gmail.com>
2019-07-16 15:25:59 +0200

    added calculator operator

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,21 @@ var add = function(number1, number2) {
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   alert(add(number1, number2));
+  var substract= function(number1, number2) {
+    return number1 - number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(substract(number1, number2));
+  var multiplication= function(number1, number2) {
+    return number1 * number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(multiplication(number1, number2));
+  var division= function(number1, number2) {
+    return number1 / number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  alert(division(number1, number2));
